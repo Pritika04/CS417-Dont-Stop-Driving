bbe2eca093e8cb125f9547dd313ccdcfe290c729
Pritika Aggarwal <pritika.aggarwal@gmail.com>
2026-04-23 08:40:41 -0500

    added looping logic

--- Assets/Scripts/RoadMover.cs
+++ Assets/Scripts/RoadMover.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
-public class RoadMover : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class RoadMover : MonoBehaviour {
+    public float speed = 15f; 
+    public float roadLength = 20f;
+    public Transform otherSegment;
+
+    void Update() {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+        if (transform.position.z <= -roadLength) {
+            float newZ = otherSegment.position.z + roadLength;
+            transform.position = new Vector3(0, 0, newZ);
+
+            RandomizeDecor();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void RandomizeDecor() {
+        // TODO: randomize trees/humanoid monster, etc every 10 seconds
     }
 }
