638837bc5fd7aebcd2caf7b77d5e486cb3665d47
Pritika Aggarwal <pritika.aggarwal@gmail.com>
2026-04-27 13:17:39 -0500

    added spatialised sound and particle burst to hitogata

--- Assets/Scripts/MosterSpawning.cs
+++ Assets/Scripts/MosterSpawning.cs
@@ -5,6 +5,10 @@ public class HitogataSpawner : MonoBehaviour {
     public float activeDuration = 5f;
     public GameObject visuals;
 
+    [Header("Juicy Feedback")]
+    public AudioSource spawnSound;
+    public ParticleSystem spawnBurst;
+
     void Start() {
         visuals.SetActive(false);
         InvokeRepeating("TrySpawn", spawnInterval, spawnInterval);
@@ -14,6 +18,12 @@ public class HitogataSpawner : MonoBehaviour {
         float x = Random.Range(-3f, 3f); 
         transform.localPosition = new Vector3(x, 0, 22f); 
         visuals.SetActive(true);
+        if (spawnSound != null) {
+            spawnSound.Play();
+        }
+        if (spawnBurst != null) {
+            spawnBurst.Play();
+        }
         Invoke("Hide", activeDuration);
     }
 
